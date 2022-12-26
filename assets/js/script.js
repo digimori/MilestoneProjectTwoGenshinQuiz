8cba7c889d3c061ef43418d945a49df67285c78d
digimori <hanaorikuu@gmail.com>
2022-12-26 20:31:26 +0000

    Had to move over to jsFiddle to make things work. getElementByIds made as I was using class instead forgetting I'm not iterating over a bunch of elements so the event listeners weren't working. Thanks Valdy for that. startGame function now works to open the rules box.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
 const answerFile = require('./answers.js');
+
+const startButton = document.getElementById("startButton");
+const howToPlay = document.getElementById("info_box");
+const exitQuiz = document.getElementById("exitQuizStart");
+const continue_btn = document.getElementById("newGame");
+const quizBox = document.getElementById("quiz");
+const results = document.getElementById("results");
+const answerList = document.getElementById("answerList")
+
+
+function startGame() {
+  howToPlay.classList.add("showBox")
+}
+
+
+
+
+function continueButton() {
+  alert("BLAAAAAAAAHHH")
+}
+document.getElementById('startButton').addEventListener("click", startGame);
+
+
+document.getElementById('newGame').addEventListener("click", continueButton)
+document.getElementById('newGamePlus').addEventListener("click", continueButton)
+document.getElementById('exitQuizStart').addEventListener("click", continueButton)
+document.getElementById('exitQuiz').addEventListener("click", continueButton)
