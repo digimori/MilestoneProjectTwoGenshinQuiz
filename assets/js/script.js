5b8418c806e53b3120e63b07eef1e29706b2e31b
digimori <hanaorikuu@gmail.com>
2022-12-27 13:39:39 +0000

    Empty functions added outlining the next parts I want to do. CSS altered as there were bugs where the pointer event on the Quiz box was set to none which was stopping the hover CSS from working on the items within the div. Cleaned up HTML in index.html to remove placeholder divs which are now initiated via a function instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const answerFile = require('./answers.js');
 
+
 const startButton = document.getElementById("startButton");
 const howToPlay = document.getElementById("info_box");
 const exitQuiz = document.getElementById("exitQuizStart");
@@ -8,24 +9,61 @@ const quizBox = document.getElementById("quiz");
 const results = document.getElementById("results");
 const answerList = document.getElementById("answerList")
 
-
 function startGame() {
-  howToPlay.classList.add("showBox");
-  startButton.classList.add("hideStart");
+howToPlay.classList.add("showBox");
+startButton.classList.add("hideStart");
 }
 
-function exitGame(){
+function exitGame() {
 howToPlay.classList.remove("showBox");
-  startButton.classList.remove("hideStart");
+startButton.classList.remove("hideStart");
 }
 
 function newGame() {
 howToPlay.classList.remove("showBox");
 quizBox.classList.add("activeQuiz");
+showQuestions(0);
+}
+
+let questionCount = 0;
+
+function showQuestions(i) {
+let questionText = document.getElementById('questionText');
+let answers = document.getElementById('answerList')
+let questionShow = '<span>' + questions[i].question + '</span>';
+let showOptions = '<div class="option"><span>' + questions[i].answer[0] + '</span></div>'
+                   + '<div class="option"><span>' + questions[i].answer[1] + '</span></div>' 
+                  + '<div class="option"><span>' + questions[i].answer[2] + '</span></div>';
+questionText.innerHTML = questionShow;
+answers.innerHTML = showOptions;
+}
+
+function nextQuestion() {
+
+}
+
+function answerClicked() {
+
+}
+
+function isCorrectAnswer() {
+
+}
+
+function incorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function showAnswer() {
+
 }
 
 function continueButton() {
-  alert("BLAAAAAAAAHHH")
+alert("BLAAAAAAAAHHH")
 }
 document.getElementById('startButton').addEventListener("click", startGame);
 document.getElementById("newGame").addEventListener("click", newGame)
