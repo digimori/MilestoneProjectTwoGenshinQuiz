d7897ba96ec14b6805cf9ca82b55d25701d6cfce
digimori <hanaorikuu@gmail.com>
2022-12-27 18:51:26 +0000

    started putting together the answer selection function. Not entirely sure this is the right way to go about it, Will review tomorrow when in a better headspace

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const answerFile = require('./answers.js');
 
 
-  
+ // --------------------Getting all the elements needed from the HTML -----------//
 
 const startButton = document.getElementById("startButton");
 const howToPlay = document.getElementById("info_box");
@@ -11,41 +11,42 @@ const quizBox = document.getElementById("quiz");
 const results = document.getElementById("results");
 const answerList = document.getElementById("answerList")
 
+//------------ Start the game functions ----------------------------------//
 
 function startGame() {
-howToPlay.classList.add("showBox");
-startButton.classList.add("hideStart");
+  howToPlay.classList.add("showBox");
+  startButton.classList.add("hideStart");
 }
 
 function exitGame() {
-howToPlay.classList.remove("showBox");
-startButton.classList.remove("hideStart");
+  howToPlay.classList.remove("showBox");
+  startButton.classList.remove("hideStart");
 }
 
 function newGame() {
-howToPlay.classList.remove("showBox");
-quizBox.classList.add("activeQuiz");
-showQuestions(0);
-counter(1);
-}
-
-//----------------------Show questions function----------
-function showQuestions(i) {
-let questionText = document.getElementById('questionText');
-let answers = document.getElementById('answerList')
-let questionShow = '<span>' + questions[i].numero + ". " + questions[i].question + '</span>';
-let showOptions = '<div class="option"><span>' + questions[i].answer[0] + '</span></div>' +
-  '<div class="option"><span>' + questions[i].answer[1] + '</span></div>' +
-  '<div class="option"><span>' + questions[i].answer[2] + '</span></div>';
-questionText.innerHTML = questionShow;
-answers.innerHTML = showOptions;
+  howToPlay.classList.remove("showBox");
+  quizBox.classList.add("activeQuiz");
+  showQuestions(0);
+  counter(1);
 }
 
 let questionCount = 1;
 let questionNum = 1;
 
-//x of 21 questions code
+//----------------------Show questions function----------//
 
+function showQuestions(i) {
+  let questionText = document.getElementById('questionText');
+  let answers = document.getElementById('answerList')
+  let questionShow = '<span>' + questions[i].numero + ". " + questions[i].question + '</span>';
+  let showOptions = '<div class="option"><span>' + questions[i].answer[0] + '</span></div>' +
+    '<div class="option"><span>' + questions[i].answer[1] + '</span></div>' +
+    '<div class="option"><span>' + questions[i].answer[2] + '</span></div>';
+  questionText.innerHTML = questionShow;
+  answers.innerHTML = showOptions;
+}
+
+//-------------------------------x of 21 questions code-------------------------//
 function counter(i) {
 const quesCount = document.getElementById("totalQuestions");
 let bottomQuestionCounter = '<span><p> ' + i + ' of ' + questions.length + '</p></span>';
@@ -57,32 +58,42 @@ quesCount.innerHTML = bottomQuestionCounter;
 
 const nextButton = document.getElementById('nextButton');
 document.getElementById("nextButton").onclick = function() {
-nextQuestion()
+  nextQuestion()
 };
 
 function nextQuestion() {
-if (questionCount <= questions.length -1) {
-  showQuestions(questionCount); 
-  questionCount++;
-  questionNum++;
-  counter(questionNum);
+  if (questionCount <= questions.length -1) {
+    showQuestions(questionCount); 
+    questionCount++;
+    questionNum++;
+    counter(questionNum);
+  } else {
+    console.log("End of questions");
+  }
+}
+
+//const iterator = questions.answer.keys();
+
+/*Answer click buttons. Use 'this'?
+function selectAnswer() {
+for (let i = 0; iterator.length; i++) {
+if(key === questions.rightAnswer) {
+this.element.style.backgroundColor = 'lightGreen'
 } else {
-  console.log("End of questions");
+this.element.style.backgroundColor = 'red'
 }
 }
+}*/
 
-function answerClicked() {
-
-}
 
 function isCorrectAnswer() {
-//turn green, add to score
-document.getElementById('').style.backgroundColor = 'lightgreen';
+  //turn green, add to score
+  document.getElementById('').style.backgroundColor = 'lightgreen';
 }
 
 function incorrectAnswer() {
 document.getElementById('').style.backgroundColor = 'red';
-//turn red, don't add, initiate showAnswer() 
+  //turn red, don't add, initiate showAnswer() 
 }
 
 function incrementScore() {
@@ -93,15 +104,16 @@ function showAnswer() {
 
 }
 
+//Tester button ---------------------------------------------------------
+
 function continueButton() {
-alert("BLAAAAAAAAHHH")
+  alert("BLAAAAAAAAHHH")
 }
 
+//------------------- Event Listeners -----------------------//
 
 document.getElementById('startButton').addEventListener("click", startGame);
 document.getElementById("newGame").addEventListener("click", newGame)
-
-
 document.getElementById('newGamePlus').addEventListener("click", continueButton)
 document.getElementById('exitQuizStart').addEventListener("click", exitGame)
 document.getElementById('exitQuiz').addEventListener("click", continueButton)
