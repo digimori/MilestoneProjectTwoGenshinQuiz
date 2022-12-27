a8080cfbd77710b80e6eed475d403231eb1956fb
digimori <hanaorikuu@gmail.com>
2022-12-27 18:15:33 +0000

    Fixed bug where next question would not initiate past the second question due to having both answer and answers as an object key and only iterating over the answer key. All of the answers are now answer in answers.js. Incrementing questions function fixed, I forgot to put the increment into the 'nextButton' function.

--- assets/js/answers.js
+++ assets/js/answers.js
@@ -1,110 +1,130 @@
+
 const questions = [{
   numero: 1,
-      question: "Which region are the Knights of Favonius located?",
-      answer: ["Khaenriah", "Mondstadt", "Inazuma"],
-      rightAnswer: 1
-    },
-    {
-    numero: 2,
-      question: "Who are the four beginner characters that you start the game with?",
-      answer: ["Venti, Zhongli, Ei, Nahida", "Albedo, Sucrose, Traveler, Bennett", "Traveler, Lisa, Amber, Kaeya"],
-      rightAnswer: 2
-    },
-    {
-      question: "What is the name of the pitstop between Mondstadt and Liyue?",
-      answers: ["Dihua Marsh", "Wangshu Inn", "Dragonspine"],
-      rightAnswer: 1,
-    },
-    {
-      question: "What is the name of your Silver haired flying companion",
-      answers: ["Paimon", "Spymon", "Byemon"],
-      rightAnswer: 0,
-    },
-    {
-      question: "What element does the traveler acquire first in Teyvat?",
-      answers: ["Anemo", "Geo", "Electro"],
-      rightAnswer: 0,
-    },
-    {
-      question: "Which island of Inazuma is known for its Aztec-like story theme?",
-      answers: ["Yashiori Island", "Watatsumi Island", "Tsurumi Island"],
-      rightAnswer: 2,
-    },
-    {
-      question: "What are the demon names of the currently confirmed archons?",
-      answers: ["Santa, Rudolf, Satan, George, Frank, Jim", "Barbatos, Morax, Beelzebuul, Buer, Focalors, Murata", "Bermuda, Borax, Baal, Beer, Folklore, Murakami"],
-      rightAnswer: 1,
-    },
-    {
-      question: "How many Fatui Harbingers were there originally (We don't talk about Signora)?",
-      answers: ["11", "9", "6"],
-      rightAnswer: 0,
-    },
-    {
-      question: "Who was it who originally sealed away the traveler and their twin?",
-      answers: ["Kiana Kaslana", "The Sustainer of Heavenly Principles", "The Principality of Celestia"],
-      rightAnswer: 1,
-    },
-    {
-      question: "What are the virtues of the currently encountered archons of Teyvat (as of 3.0)?",
-      answers: ["Love, Justice, Wisdom, War", "Justice, Transience, Freedom, War", "Freedom, Contracts, Eternity, Wisdom"],
-      rightAnswer: 2,
-    },
-    {
-      question: "The Tianquan of the Liyue Qixing is?",
-      answers: ["Keqing", "Qiqi", "Ningguang"],
-      rightAnswer: 2,
-    },
-    {
-      question: "What is the name of the cell in which Nahida was imprisoned?",
-      answers: ["Sanctuary of Samsara", "Sanctuary of Suresthana", "Sanctuary of Shamshir"],
-      rightAnswer: 1,
-    },
-    {
-      question: "Who is the First descender?",
-      answers: ["Celstia", "Phanes", "Traveler"],
-      rightAnswer: 1,
-    },
-    {
-      question: "What is the name of the princess of Sal Vindagnyr's protector?",
-      answers: ["Irminsul", "Imunlaukr", "Imminent"],
-      rightAnswer: 1,
-    },
-    {
-      question: "Who was responsible for the sacrifice of King Deshret?",
-      answers: ["Greater Lord Rukkhedevata", "Himself", "Lesser Lord Kusanali"],
-      rightAnswer: 1,
-    },
-    {
-      question: "Before the First Who Came, who, or what, were the original inhabitants of Teyvat?",
-      answers: ["Celestia", "Orobashi", "Dragonheirs of the Depths"],
-      rightAnswer: 2,
-    },
-    {
-      question: "What is the name of Albedo's brother, also created by Rhinedottir?",
-      answers: ["Durin", "Dvalin", "Daran"],
-      rightAnswer: 0,
-    },
-    {
-      question: "What is the name of the creator of the Dainichi Mikoshi?",
-      answers: ["Aberaku", "Supada", "Daimon"],
-      rightAnswer: 0,
-    },
-    {
-      question: "The origin of the Black Serpent Knights is?",
-      answers: ["The Chasm", "Enkanomiya", "Khaenriah"],
-      rightAnswer: 2,
-    },
-    {
-      question: "What are the names of the Moon Sisters?",
-      answers: ["Seelie, Hili, Canon", "Aria, Sonnet, Canon", "Aria, Seelie, Hili"],
-      rightAnswer: 1,
-    },
-    {
-      question: "The Guili Assembly of Liyue was ruled by which god?",
-      answers: ["Zhongli", "Havria", "Guizhong"],
-      rightAnswer: 2,
-    },
-  ];
+  question: "Which region are the Knights of Favonius located?",
+  answer: ["Khaenriah", "Mondstadt", "Inazuma"],
+  rightAnswer: 1
+},
+{
+  numero: 2,
+  question: "Who are the four beginner characters that you start the game with?",
+  answer: ["Venti, Zhongli, Ei, Nahida", "Albedo, Sucrose, Traveler, Bennett", "Traveler, Lisa, Amber, Kaeya"],
+  rightAnswer: 2
+},
+{
+numero: 3,
+  question: "What is the name of the pitstop between Mondstadt and Liyue?",
+  answer: ["Dihua Marsh", "Wangshu Inn", "Dragonspine"],
+  rightAnswer: 1,
+},
+{
+numero: 4,
+  question: "What is the name of your Silver haired flying companion",
+  answer: ["Paimon", "Spymon", "Byemon"],
+  rightAnswer: 0,
+},
+{
+numero: 5,
+  question: "What element does the traveler acquire first in Teyvat?",
+  answer: ["Anemo", "Geo", "Electro"],
+  rightAnswer: 0,
+},
+{
+numero: 6,
+  question: "Which island of Inazuma is known for its Aztec-like story theme?",
+  answer: ["Yashiori Island", "Watatsumi Island", "Tsurumi Island"],
+  rightAnswer: 2,
+},
+{
+numero: 7,
+  question: "What are the demon names of the currently confirmed archons?",
+  answer: ["Santa, Rudolf, Satan, George, Frank, Jim", "Barbatos, Morax, Beelzebuul, Buer, Focalors, Murata", "Bermuda, Borax, Baal, Beer, Folklore, Murakami"],
+  rightAnswer: 1,
+},
+{
+numero: 8,
+  question: "How many Fatui Harbingers were there originally (We don't talk about Signora)?",
+  answer: ["11", "9", "6"],
+  rightAnswer: 0,
+},
+{
+numero: 9,
+  question: "Who was it who originally sealed away the traveler and their twin?",
+  answer: ["Kiana Kaslana", "The Sustainer of Heavenly Principles", "The Principality of Celestia"],
+  rightAnswer: 1,
+},
+{
+numero: 10,
+  question: "What are the virtues of the currently encountered archons of Teyvat (as of 3.0)?",
+  answer: ["Love, Justice, Wisdom, War", "Justice, Transience, Freedom, War", "Freedom, Contracts, Eternity, Wisdom"],
+  rightAnswer: 2,
+},
+{
+numero: 11,
+  question: "The Tianquan of the Liyue Qixing is?",
+  answer: ["Keqing", "Qiqi", "Ningguang"],
+  rightAnswer: 2,
+},
+{
+numero: 12,
+  question: "What is the name of the cell in which Nahida was imprisoned?",
+  answer: ["Sanctuary of Samsara", "Sanctuary of Suresthana", "Sanctuary of Shamshir"],
+  rightAnswer: 1,
+},
+{
+numero: 13,
+  question: "Who is the First descender?",
+  answer: ["Celstia", "Phanes", "Traveler"],
+  rightAnswer: 1,
+},
+{
+numero: 14,
+  question: "What is the name of the princess of Sal Vindagnyr's protector?",
+  answer: ["Irminsul", "Imunlaukr", "Imminent"],
+  rightAnswer: 1,
+},
+{
+numero: 15,
+  question: "Who was responsible for the sacrifice of King Deshret?",
+  answer: ["Greater Lord Rukkhedevata", "Himself", "Lesser Lord Kusanali"],
+  rightAnswer: 1,
+},
+{
+numero: 16,
+  question: "Before the First Who Came, who, or what, were the original inhabitants of Teyvat?",
+  answer: ["Celestia", "Orobashi", "Dragonheirs of the Depths"],
+  rightAnswer: 2,
+},
+{
+numero: 17,
+  question: "What is the name of Albedo's brother, also created by Rhinedottir?",
+  answer: ["Durin", "Dvalin", "Daran"],
+  rightAnswer: 0,
+},
+{
+numero: 18,
+  question: "What is the name of the creator of the Dainichi Mikoshi?",
+  answer: ["Aberaku", "Supada", "Daimon"],
+  rightAnswer: 0,
+},
+{
+numero: 19,
+  question: "The origin of the Black Serpent Knights is?",
+  answer: ["The Chasm", "Enkanomiya", "Khaenriah"],
+  rightAnswer: 2,
+},
+{
+numero: 20,
+  question: "What are the names of the Moon Sisters?",
+  answer: ["Seelie, Hili, Canon", "Aria, Sonnet, Canon", "Aria, Seelie, Hili"],
+  rightAnswer: 1,
+},
+{
+numero: 21,
+  question: "The Guili Assembly of Liyue was ruled by which god?",
+  answer: ["Zhongli", "Havria", "Guizhong"],
+  rightAnswer: 2,
+},
+];
 
 module.exports = { questions };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,90 +1,107 @@
 const answerFile = require('./answers.js');
 
-    
-    
-    const startButton = document.getElementById("startButton");
-    const howToPlay = document.getElementById("info_box");
-    const exitQuiz = document.getElementById("exitQuizStart");
-    const continue_btn = document.getElementById("newGame");
-    const quizBox = document.getElementById("quiz");
-    const results = document.getElementById("results");
-    const answerList = document.getElementById("answerList")
-    
-    
-    function startGame() {
-      howToPlay.classList.add("showBox");
-      startButton.classList.add("hideStart");
-    }
-    
-    function exitGame() {
-      howToPlay.classList.remove("showBox");
-      startButton.classList.remove("hideStart");
-    }
-    
-    function newGame() {
-      howToPlay.classList.remove("showBox");
-      quizBox.classList.add("activeQuiz");
-      showQuestions(0);
-    }
-    
-    
-    function showQuestions(i) {
-      let questionText = document.getElementById('questionText');
-      let answers = document.getElementById('answerList')
-      let questionShow = '<span>' + questions[i].numero + ". " + questions[i].question + '</span>';
-      let showOptions = '<div class="option"><span>' + questions[i].answer[0] + '</span></div>'
-                                 + '<div class="option"><span>' + questions[i].answer[1] + '</span></div>' 
-                                          + '<div class="option"><span>' + questions[i].answer[2] + '</span></div>';
-      questionText.innerHTML = questionShow;
-      answers.innerHTML = showOptions;
-    }
-    
-    let questionCount = 0;
-    const nextButton = document.getElementById('nextButton');
-    const quesCount = document.getElementById("totalQuestions");
-    let totalQuestions =  '<p><span>' + questionCount + '</span> of 21</p>';
-    
-    
-    document.getElementById("nextButton").onclick = function() {nextQuestion()};
-     
-     function nextQuestion() {
-      if(questionCount < questions.length) {
-    questionCount++; 
-    showQuestions(questionCount);
-    } else {
-    console.log("End of questions");
-    }}
-    
-    function answerClicked() {
-    
-    }
-    
-    function isCorrectAnswer() {
-    
-    }
-    
-    function incorrectAnswer() {
-    
-    }
-    
-    function incrementScore() {
-    
-    }
-    
-    function showAnswer() {
-    
-    }
-    
-    function continueButton() {
-      alert("BLAAAAAAAAHHH")
-    }
-    
-    
-    document.getElementById('startButton').addEventListener("click", startGame);
-    document.getElementById("newGame").addEventListener("click", newGame)
-    
-    
-    document.getElementById('newGamePlus').addEventListener("click", continueButton)
-    document.getElementById('exitQuizStart').addEventListener("click", exitGame)
-    document.getElementById('exitQuiz').addEventListener("click", continueButton)
-    
+
+  
+
+const startButton = document.getElementById("startButton");
+const howToPlay = document.getElementById("info_box");
+const exitQuiz = document.getElementById("exitQuizStart");
+const continue_btn = document.getElementById("newGame");
+const quizBox = document.getElementById("quiz");
+const results = document.getElementById("results");
+const answerList = document.getElementById("answerList")
+
+
+function startGame() {
+howToPlay.classList.add("showBox");
+startButton.classList.add("hideStart");
+}
+
+function exitGame() {
+howToPlay.classList.remove("showBox");
+startButton.classList.remove("hideStart");
+}
+
+function newGame() {
+howToPlay.classList.remove("showBox");
+quizBox.classList.add("activeQuiz");
+showQuestions(0);
+counter(1);
+}
+
+//----------------------Show questions function----------
+function showQuestions(i) {
+let questionText = document.getElementById('questionText');
+let answers = document.getElementById('answerList')
+let questionShow = '<span>' + questions[i].numero + ". " + questions[i].question + '</span>';
+let showOptions = '<div class="option"><span>' + questions[i].answer[0] + '</span></div>' +
+  '<div class="option"><span>' + questions[i].answer[1] + '</span></div>' +
+  '<div class="option"><span>' + questions[i].answer[2] + '</span></div>';
+questionText.innerHTML = questionShow;
+answers.innerHTML = showOptions;
+}
+
+let questionCount = 1;
+let questionNum = 1;
+
+//x of 21 questions code
+
+function counter(i) {
+const quesCount = document.getElementById("totalQuestions");
+let bottomQuestionCounter = '<span><p> ' + i + ' of ' + questions.length + '</p></span>';
+quesCount.innerHTML = bottomQuestionCounter;
+}
+
+
+// -------------------Button and function for next question---------------------
+
+const nextButton = document.getElementById('nextButton');
+document.getElementById("nextButton").onclick = function() {
+nextQuestion()
+};
+
+function nextQuestion() {
+if (questionCount <= questions.length -1) {
+  showQuestions(questionCount); 
+  questionCount++;
+  questionNum++;
+  counter(questionNum);
+} else {
+  console.log("End of questions");
+}
+}
+
+function answerClicked() {
+
+}
+
+function isCorrectAnswer() {
+//turn green, add to score
+document.getElementById('').style.backgroundColor = 'lightgreen';
+}
+
+function incorrectAnswer() {
+document.getElementById('').style.backgroundColor = 'red';
+//turn red, don't add, initiate showAnswer() 
+}
+
+function incrementScore() {
+
+}
+
+function showAnswer() {
+
+}
+
+function continueButton() {
+alert("BLAAAAAAAAHHH")
+}
+
+
+document.getElementById('startButton').addEventListener("click", startGame);
+document.getElementById("newGame").addEventListener("click", newGame)
+
+
+document.getElementById('newGamePlus').addEventListener("click", continueButton)
+document.getElementById('exitQuizStart').addEventListener("click", exitGame)
+document.getElementById('exitQuiz').addEventListener("click", continueButton)
