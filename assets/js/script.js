647d46036732738772abf03d8a2bde9566f2da60
digimori <hanaorikuu@gmail.com>
2022-12-28 19:39:12 +0000

    Imported answers into script.js file. Trying to fix some bugs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,133 @@ $(document).ready(function(){
   console.log("Ready");
   })
   
-  const answerFile = import('./answers.js')
+  const questions = [{
+    numero: 1,
+    question: "Which region are the Knights of Favonius located?",
+    answer: ["Khaenriah", "Mondstadt", "Inazuma"],
+    rightAnswer: "Mondstadt",
+  },
+  {
+    numero: 2,
+    question: "Who are the four beginner characters that you start the game with?",
+    answer: ["Venti, Zhongli, Ei, Nahida", "Albedo, Sucrose, Traveler, Bennett", "Traveler, Lisa, Amber, Kaeya"],
+    rightAnswer: "Traveler, Lisa, Amber, Kaeya",
+  },
+  {
+    numero: 3,
+    question: "What is the name of the pitstop between Mondstadt and Liyue?",
+    answer: ["Dihua Marsh", "Wangshu Inn", "Dragonspine"],
+    rightAnswer: "Wangshu Inn",
+  },
+  {
+    numero: 4,
+    question: "What is the name of your Silver haired flying companion",
+    answer: ["Paimon", "Spymon", "Byemon"],
+    rightAnswer: "Paimon",
+  },
+  {
+    numero: 5,
+    question: "What element does the traveler acquire first in Teyvat?",
+    answer: ["Anemo", "Geo", "Electro"],
+    rightAnswer: "Anemo",
+  },
+  {
+    numero: 6,
+    question: "Which island of Inazuma is known for its Aztec-like story theme?",
+    answer: ["Yashiori Island", "Watatsumi Island", "Tsurumi Island"],
+    rightAnswer: "Tsurumi Island",
+  },
+  {
+    numero: 7,
+    question: "What are the demon names of the currently confirmed archons?",
+    answer: ["Santa, Rudolf, Satan, George, Frank, Jim", "Barbatos, Morax, Beelzebuul, Buer, Focalors, Murata", "Bermuda, Borax, Baal, Beer, Folklore, Murakami"],
+    rightAnswer: "Barbatos, Morax, Beelzebuul, Buer, Focalors, Murata",
+  },
+  {
+    numero: 8,
+    question: "How many Fatui Harbingers were there originally (We don't talk about Signora)?",
+    answer: ["11", "9", "6"],
+    rightAnswer: "11",
+  },
+  {
+    numero: 9,
+    question: "Who was it who originally sealed away the traveler and their twin?",
+    answer: ["Kiana Kaslana", "The Sustainer of Heavenly Principles", "The Principality of Celestia"],
+    rightAnswer: "The Sustainer of Heavenly Principles",
+  },
+  {
+    numero: 10,
+    question: "What are the virtues of the currently encountered archons of Teyvat (as of 3.0)?",
+    answer: ["Love, Justice, Wisdom, War", "Justice, Transience, Freedom, War", "Freedom, Contracts, Eternity, Wisdom"],
+    rightAnswer: "Freedom, Contracts, Eternity, Wisdom",
+  },
+  {
+    numero: 11,
+    question: "The Tianquan of the Liyue Qixing is?",
+    answer: ["Keqing", "Qiqi", "Ningguang"],
+    rightAnswer: "Ningguang",
+  },
+  {
+    numero: 12,
+    question: "What is the name of the cell in which Nahida was imprisoned?",
+    answer: ["Sanctuary of Samsara", "Sanctuary of Suresthana", "Sanctuary of Shamshir"],
+    rightAnswer: "Sanctuary of Suresthana",
+  },
+  {
+    numero: 13,
+    question: "Who is the First descender?",
+    answer: ["Celstia", "Phanes", "Traveler"],
+    rightAnswer: "Phanes",
+  },
+  {
+    numero: 14,
+    question: "What is the name of the princess of Sal Vindagnyr's protector?",
+    answer: ["Irminsul", "Imunlaukr", "Imminent"],
+    rightAnswer: "Imunlaukr",
+  },
+  {
+    numero: 15,
+    question: "Who was responsible for the sacrifice of King Deshret?",
+    answer: ["Greater Lord Rukkhedevata", "Himself", "Lesser Lord Kusanali"],
+    rightAnswer: "Himself",
+  },
+  {
+    numero: 16,
+    question: "Before the First Who Came, who, or what, were the original inhabitants of Teyvat?",
+    answer: ["Celestia", "Orobashi", "Dragonheirs of the Depths"],
+    rightAnswer: "Dragonheirs of the Depths",
+  },
+  {
+    numero: 17,
+    question: "What is the name of Albedo's brother, also created by Rhinedottir?",
+    answer: ["Durin", "Dvalin", "Daran"],
+    rightAnswer: "Durin",
+  },
+  {
+    numero: 18,
+    question: "What is the name of the creator of the Dainichi Mikoshi?",
+    answer: ["Aberaku", "Supada", "Daimon"],
+    rightAnswer: "Aberaku",
+  },
+  {
+    numero: 19,
+    question: "The origin of the Black Serpent Knights is?",
+    answer: ["The Chasm", "Enkanomiya", "Khaenriah"],
+    rightAnswer: "Khaenriah",
+  },
+  {
+    numero: 20,
+    question: "What are the names of the Moon Sisters?",
+    answer: ["Seelie, Hili, Canon", "Aria, Sonnet, Canon", "Aria, Seelie, Hili"],
+    rightAnswer: "Aria, Sonnet, Canon",
+  },
+  {
+    numero: 21,
+    question: "The Guili Assembly of Liyue was ruled by which god?",
+    answer: ["Zhongli", "Havria", "Guizhong"],
+    rightAnswer: "Guizhong",
+  },
+  ];
 
   // --------------------Getting all the elements needed from the HTML -----------//
   
